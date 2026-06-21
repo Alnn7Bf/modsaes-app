1ded4ecb2c8c0592a81814ebb354bece8a5381a7
Alnn7Bf <alanubf2018@gmail.com>
2026-06-20 21:36:23 -0600

    v1.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -2,25 +2,12 @@ import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
-import { visualizer } from 'rollup-plugin-visualizer'
 
 export default defineConfig({
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {src: 'README.md', dest: ''}
-      ]
-    }),
-    visualizer({
-      filename: "stats.html",
-      open: false,
-      gzipSize: true,
-      brotliSize: true
-    })
+    tailwindcss()
   ],
   build: {
     rollupOptions: {
